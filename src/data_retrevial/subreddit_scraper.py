98d62bb9bc22c04a65be8e884da826b945796adf
CharlesJub <charlie.jubera@gmail.com>
2025-03-06 13:17:08 -0500

    UPDATE SCRAPER

--- src/data_retrevial/subreddit_scraper.py
+++ src/data_retrevial/subreddit_scraper.py
@@ -3,7 +3,7 @@ import praw
 reddit = praw.Reddit(
     client_id=secret.client_id,
     client_secret=secret.api_key,
-    user_agent="SubTopicClustering:V1.0 (by u/Rektemlord)",
+    user_agent="SubTopicClustering:V1.0",
 )
 
 print(reddit.read_only)
